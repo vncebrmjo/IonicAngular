e8269f667e3e92ef48ac0eaa7ceac6c97999ba74
vncebrmjo <bermejovince31@gmail.com>
2025-06-26 10:39:53 +0800

    Initial Commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { AppComponent } from "../app.component";
+import { HeaderComponent } from "../components/header/header.component";
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, AppComponent, HeaderComponent],
 })
 export class HomePage {
   constructor() {}
